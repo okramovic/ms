420f7c28457063a1efe82bef2ad4d3748a3cbb11
marko bogdanovic <okram@protonmail.ch>
2021-07-24 15:29:00 +0200

    add focus to new answer option text-input when mounted

--- src/components/AnswerOption/AnswerOption.js
+++ src/components/AnswerOption/AnswerOption.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import {CloseIcon} from '../CloseIcon/CloseIcon';
 
-export const AnswerOption = ({choice, index, onOptionTextChange, onRemoveOptionClick})=>(
+export const AnswerOption = ({choice, index, onOptionTextChange, onRemoveOptionClick})=>{
     
-    <li key={index} className="optionAnswerContainer width100percent flex alignItemsCenter">
-        <div className="fakeRadio"/>
-        <input type="text" value={choice} placeholder={`Option ${index+1}`} onChange={ev => onOptionTextChange(ev, index) }/>
-        <button className="removeButton" onClick={onRemoveOptionClick}>
-            <CloseIcon/>
-        </button>
-    </li>
-)
+    const textInputRef = useRef(null);
+
+    useEffect(()=>{
+        textInputRef.current && textInputRef.current.focus();
+    },[])
+
+    return(
+        <li key={index} className="optionAnswerContainer width100percent flex alignItemsCenter">
+            <div className="fakeRadio"/>
+            <input type="text" value={choice} placeholder={`Option ${index+1}`} 
+                ref={textInputRef}
+                onChange={ev => onOptionTextChange(ev, index) }
+            />
+            <button className="removeButton" onClick={onRemoveOptionClick}>
+                <CloseIcon/>
+            </button>
+        </li>
+    )
+}
